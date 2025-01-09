f72e43749c48e67c3c3878db8f868a21806ab905
Gabriella Calvano <gabcalvano@gmail.com>
2025-01-09 11:21:50 +0100

    commit not working

--- JS/main.js
+++ JS/main.js
@@ -32,3 +32,29 @@ const tasks = [
 
 renderTask();
 
+// Busca la tarea que tenga el id `taskId` en el array `tasks`
+// Una vez que has obtenido la tarea, actualiza la propiedad `completed`
+// Pinta de nuevo las tareas en el html
+
+const handleClickList = (event) => {
+  const taskId = parseInt(event.target.id);
+  if (!taskId) {
+    return
+  } else {
+    for (const task of tasks) {
+      const checkedTask = tasks.find((task) => task.id === taskId);
+      console.log(checkedTask);
+
+    }
+    
+  }
+
+};
+
+taskList.addEventListener('click', handleClickList);
+
+
+
+
+
+  
